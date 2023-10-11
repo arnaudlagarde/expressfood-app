508e8f0e6a280e22f7b3617bf2eafcb2431abe19
arnaudlagarde <xx-momo@hotmail.fr>
2023-10-11 20:50:14 +0200

    fix ?

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -97,13 +96,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Database settings
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "djongo",
+        "ENFORCE_SCHEMA": False,
+        "NAME": "Restaurant",  # Your MongoDB database name
+        "CLIENT": {
+            "host": "mongodb+srv://arnaudlagarde:arnaud1234@cluster0.gnbsw2l.mongodb.net/Restaurant?retryWrites=true&w=majority",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
