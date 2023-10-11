889a918bb602c7de92af29a421850a696ac98322
arnaudlagarde <xx-momo@hotmail.fr>
2023-10-11 11:42:52 +0200

    Home improved

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,39 +1,20 @@
 import React from "react";
-// import { Container, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./Home.css";
 
 function Home() {
   return (
-    // <div class="container">
-    //   <img
-    //     className="imagSize"
-    //     src=""
-    //     alt="Snow"
-    //   />
-
-    //   <div className="centered">Centered</div>
-    // </div>
-    <nav className="navbar bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="{home}">
-          <img
-            src="https://images.pexels.com/photos/326311/pexels-photo-326311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Logo"
-            width="1350"
-            height="500"
-            className="d-inline-block align-text-top"
-          />
-          <div className="centered">
-            <h1> NachoNova Grill</h1>
-            <h3>Un menu à enporter</h3>
-            <p>25 rue Claude Tillier, 75012 Paris.</p>
-            <p>
-              <a href="tel:+Tel : +33 1 55 43 26 65">Tel : 01 55 43 26 65</a>
-            </p>
-            <button>Commandez maintenant</button>
-          </div>
+    <div className="background-image">
+      <h1 className="title">NachoNova Grill</h1>
+      <h3>Un menu à emporter</h3>
+      <p>25 rue Claude Tillier, 75012 Paris.</p>
+      <p>
+        <a href="tel:+33155432665" style={{ color: "white" }}>
+          Tel : 01 55 43 26 65
         </a>
-      </div>
-    </nav>
+      </p>
+      <Button variant="primary">Commandez maintenant</Button>
+    </div>
   );
 }
 
