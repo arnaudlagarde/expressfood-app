a3a552bffa759b58f75f499c1e98108d03599626
NadimHipssi <121816205+NadimHipssi@users.noreply.github.com>
2023-10-12 01:03:35 +0200

    Connexion fonctionne

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -18,7 +18,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const jsonData = JSON.stringify(formData); // Convertir les données en JSON
+            const jsonData = JSON.stringify(formData);
 
             const response = await fetch("http://localhost:8000/connexion", {
                 method: "POST",
@@ -29,22 +29,42 @@ function Login() {
             });
 
             if (response.status === 200) {
-                console.log("Connexion réussie");
-                // Redirigez l'utilisateur ou affichez un message de succès.
+                const user = await response.json();
+                console.log('Connexion réussie', user);
+                // Redirigez l'utilisateur ou effectuez d'autres actions après la connexion réussie.
             } else {
-                console.log("Échec de la connexion");
+                console.log('Échec de la connexion');
                 // La connexion a échoué, affichez un message d'erreur.
             }
         } catch (error) {
             console.error("Erreur lors de la connexion :", error);
         }
-    }
+    };
 
     return (
         <Container>
             <h2>Connexion</h2>
             <Form onSubmit={handleSubmit}>
-                {/* ... Autres champs de formulaire ... */}
+                <Form.Group className="mb-3">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Mot de passe</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </Form.Group>
                 <Button variant="primary" type="submit">
                     Se connecter
                 </Button>
