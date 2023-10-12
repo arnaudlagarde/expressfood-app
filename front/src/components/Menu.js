13d6bad0921f19545192ed0bfe078b1145b5c228
arnaudlagarde <xx-momo@hotmail.fr>
2023-10-12 11:15:06 +0200

    Plats du jour enhanced

--- front/src/components/Menu.js
+++ front/src/components/Menu.js
@@ -1,66 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { Container, Card, Button } from "react-bootstrap";
-import axios from "axios"; // Importez Axios
-
-//import platsDuJourData from "../data/platsDuJour.json";
+import axios from "axios";
+import "./Menu.css"; // Add your custom styles here for hover effect
 
 function Menu() {
-  const [platsDuJour, setPlatsDuJour] = useState([]); // État pour stocker les plats du jour
-  const [cart, setCart] = useState([]); // État pour gérer le panier
-  const [quantities, setQuantities] = useState({}); // État pour gérer les quantités
-
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"; // Vérifiez si l'utilisateur est connecté
-  
+  const [platsDuJour, setPlatsDuJour] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [quantities, setQuantities] = useState({});
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
   const api = axios.create({
-    baseURL: "http://localhost:8000", // Assurez-vous que l'URL correspond à l'endroit où votre serveur Express est en cours d'exécution
+    baseURL: "http://localhost:8000",
   });
 
   const addToCart = (plat) => {
-    // Fonction pour ajouter un plat au panier
     const updatedCart = [...cart, plat];
     setCart(updatedCart);
   };
 
   const updateQuantity = (plat, quantity) => {
-    // Fonction pour mettre à jour la quantité
     setQuantities({ ...quantities, [plat._id]: quantity });
   };
 
   const handleCommande = () => {
-    // Récupérez l'ID du client depuis le localStorage
     const clientId = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user"))._id : null;
 
     if (!clientId) {
-      console.error("ID du client non trouvé dans le localStorage.");
+      console.error("Client ID not found in localStorage.");
       return;
     }
 
-    // Récupérez la liste des livreurs libres
     api.get("/livreurs-libres")
       .then((response) => {
         const livreursLibres = response.data;
 
         if (livreursLibres.length === 0) {
-          console.error("Aucun livreur libre disponible.");
+          console.error("No available delivery drivers.");
           return;
         }
 
-        // Prenez le premier livreur libre
         const premierLivreurLibre = livreursLibres[0];
 
-        // Créez un objet qui représente la nouvelle commande avec le livreur attribué
         const nouvelleCommande = {
-          clientId: clientId, // Remplacez par l'ID du client concerné
+          clientId: clientId,
           plats: cart.map((plat) => ({ platId: plat._id, quantite: quantities[plat._id] || 1 })),
           dateCommande: new Date(),
-          livreurId: premierLivreurLibre._id, // Utilisez l'ID du premier livreur libre
+          livreurId: premierLivreurLibre._id,
         };
 
-        // Envoyez la demande au serveur pour créer la commande
         api.post("/commandes", nouvelleCommande)
           .then((response) => {
             console.log("Commande créée avec succès !", response.data);
-            // Réinitialisez le panier et les quantités après la commande
             setCart([]);
             setQuantities({});
           })
@@ -74,31 +63,29 @@ function Menu() {
   };
 
   useEffect(() => {
-    // Effectuez une requête pour obtenir les plats du jour depuis la route
     api.get("/plats_du_jour")
       .then((response) => {
-        // Mettez à jour l'état avec les données récupérées
         setPlatsDuJour(response.data);
       })
       .catch((error) => {
         console.error("Erreur lors de la récupération des plats du jour :", error);
       });
-  }, []); // Utilisez une dépendance vide pour effectuer la requête une seule fois
-
+  }, []);
 
   return (
     <div>
       <Container>
         <h2>Plats du Jour</h2>
         <div className="row">
-          {platsDuJour.map((plat, index) => (
-            <div key={index} className="col-md-4">
+          {platsDuJour.map((plat) => (
+            <div key={plat._id} className="col-md-4">
               <Card className="card-space">
                 <Card.Img
                   variant="top"
                   src={plat.image}
                   height="250"
                   alt={plat.nom}
+                  className="dish-image" // Add this class for hover effect
                 />
                 <Card.Body>
                   <Card.Title>{plat.nom}</Card.Title>
@@ -106,42 +93,41 @@ function Menu() {
                   <hr />
                   <Card.Text>Prix : {plat.prix} €</Card.Text>
                 </Card.Body>
-                {isLoggedIn ? ( // Condition pour afficher uniquement si l'utilisateur est connecté
-                <>
-                  <Button variant="primary" onClick={() => addToCart(plat)}>
-                    Commander
-                  </Button>
-                  <input
-                    type="number"
-                    value={quantities[plat._id] || 1}
-                    onChange={(e) => updateQuantity(plat, e.target.value)}
-                  />
-                </>
-                ) : null}
+                {isLoggedIn && (
+                  <>
+                    <Button variant="info" onClick={() => addToCart(plat)}> {/* Change to gray-blue color */}
+                      Ajouter au panier
+                    </Button>
+                    <input
+                      type="number"
+                      value={quantities[plat._id] || 1}
+                      onChange={(e) => updateQuantity(plat, e.target.value)}
+                    />
+                  </>
+                )}
               </Card>
             </div>
           ))}
         </div>
       </Container>
       <br />
-      {isLoggedIn ? ( // Condition pour afficher uniquement si l'utilisateur est connecté
-      <div>
-        <h2>Panier</h2>
-        {cart.map((plat) => (
-          <div key={plat._id}>
-            {plat.nom} - Quantité : {quantities[plat._id] || 1}
-          </div>
-        ))}
-      </div>
-      ) : null}
-      {isLoggedIn ? ( // Condition pour afficher uniquement si l'utilisateur est connecté
-        <Button variant="success" onClick={handleCommande}>
-            Confirmer la commande
-        </Button>
-      ) : null}
+      {isLoggedIn && (
+        <div>
+          <h2>Panier</h2>
+          {cart.map((plat) => (
+            <div key={plat._id}>
+              {plat.nom} - Quantité : {quantities[plat._id] || 1}
+            </div>
+          ))}
+          {cart.length > 0 && (
+            <Button variant="success" onClick={handleCommande}>
+              Confirmer la commande
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   );
-
 }
 
 export default Menu;
