375040ccc31908b247788102f77a84cf6dcf0ea7
NadimHipssi <121816205+NadimHipssi@users.noreply.github.com>
2023-10-12 03:31:16 +0200

    Ajout du Panier au menu (mais pas encore d'envoi a la base)

--- front/src/components/Menu.js
+++ front/src/components/Menu.js
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Button } from "react-bootstrap";
 import axios from "axios"; // Importez Axios
 
 //import platsDuJourData from "../data/platsDuJour.json";
 
 function Menu() {
   const [platsDuJour, setPlatsDuJour] = useState([]); // État pour stocker les plats du jour
+  const [cart, setCart] = useState([]); // État pour gérer le panier
+  const [quantities, setQuantities] = useState({}); // État pour gérer les quantités
+
+  
   const api = axios.create({
     baseURL: "http://localhost:8000", // Assurez-vous que l'URL correspond à l'endroit où votre serveur Express est en cours d'exécution
   });
 
+  const addToCart = (plat) => {
+    // Fonction pour ajouter un plat au panier
+    const updatedCart = [...cart, plat];
+    setCart(updatedCart);
+  };
+
+  const updateQuantity = (plat, quantity) => {
+    // Fonction pour mettre à jour la quantité
+    setQuantities({ ...quantities, [plat._id]: quantity });
+  };
+
+  const handleCommande = () => {
+    // Cette fonction doit être appelée lorsque le client confirme la commande
+    // Vous enverrez les données du panier au serveur ici
+  };
 
   useEffect(() => {
     // Effectuez une requête pour obtenir les plats du jour depuis la route
@@ -44,12 +63,31 @@ function Menu() {
                   <hr />
                   <Card.Text>Prix : {plat.prix} €</Card.Text>
                 </Card.Body>
+                <Button variant="primary" onClick={() => addToCart(plat)}>
+                  Commander
+                </Button>
+                <input
+                  type="number"
+                  value={quantities[plat._id] || 1}
+                  onChange={(e) => updateQuantity(plat, e.target.value)}
+                />
               </Card>
             </div>
           ))}
         </div>
       </Container>
       <br />
+      <div>
+        <h2>Panier</h2>
+        {cart.map((plat) => (
+          <div key={plat._id}>
+            {plat.nom} - Quantité : {quantities[plat._id] || 1}
+          </div>
+        ))}
+      </div>
+      <Button variant="success" onClick={handleCommande}>
+          Confirmer la commande
+      </Button>
     </div>
   );
 
