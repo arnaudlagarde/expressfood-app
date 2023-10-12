5c7b21f1ddbf9cd4f25f43c1686118faf3666358
arnaudlagarde <xx-momo@hotmail.fr>
2023-10-12 17:17:43 +0200

    Update Profile.js

--- front/src/components/Profile.js
+++ front/src/components/Profile.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 
 function Profile() {
     const [formData, setFormData] = useState({});
+    const [updateStatus, setUpdateStatus] = useState(null);
+    const [alertMessage, setAlertMessage] = useState('');
 
     // Fetch the user's email from local storage
-    const user = JSON.parse(localStorage.getItem('user') || "{}");
+    const user = JSON.parse(localStorage.getItem('user') || {});
     const userEmail = user.email;
 
     useEffect(() => {
@@ -30,9 +32,15 @@ function Profile() {
                 console.log('Profile updated', response.data);
                 // Update local storage with the updated profile data
                 localStorage.setItem('user', JSON.stringify(response.data));
+                // Set success alert
+                setUpdateStatus('success');
+                setAlertMessage('Profil mis à jour avec succès !');
             })
             .catch(error => {
                 console.error('Error updating profile', error);
+                // Set error alert
+                setUpdateStatus('error');
+                setAlertMessage('Erreur lors de la mise à jour du profil.');
             });
     };
 
@@ -43,22 +51,35 @@ function Profile() {
 
     return (
         <div>
-            <h1>Profile of {formData.firstName} {formData.lastName}</h1>
+            <h1>Bienvenue {formData.firstName} {formData.lastName} !</h1>
             <Form onSubmit={handleSubmit}>
+                {updateStatus === 'success' && (
+                    <Alert variant="success">{alertMessage}</Alert>
+                )}
+                {updateStatus === 'error' && (
+                    <Alert variant="danger">{alertMessage}</Alert>
+                )}
                 <Form.Group>
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" name="email" value={formData.email || ''} onChange={handleChange} />
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label>First Name</Form.Label>
+                    <Form.Label>Prénom</Form.Label>
                     <Form.Control type="text" name="firstName" value={formData.firstName || ''} onChange={handleChange} />
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label>Last Name</Form.Label>
+                    <Form.Label>Nom de famille</Form.Label>
                     <Form.Control type="text" name="lastName" value={formData.lastName || ''} onChange={handleChange} />
                 </Form.Group>
-                {/* Add other fields here as needed */}
-                <Button type="submit">Update</Button>
+                <Form.Group>
+                    <Form.Label>Téléphone</Form.Label>
+                    <Form.Control type="text" name="phone" value={formData.phone || ''} onChange={handleChange} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Adresse</Form.Label>
+                    <Form.Control type="text" name="address" value={formData.address || ''} onChange={handleChange} />
+                </Form.Group>
+                <Button type="submit">Mettre à jour</Button>
             </Form>
         </div>
     );
